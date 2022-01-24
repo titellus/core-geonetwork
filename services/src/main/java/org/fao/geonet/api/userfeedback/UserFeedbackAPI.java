4d6939ed59076604b02368e6a9afdeada5f46058
François Prunayre <fx.prunayre@gmail.com>
2022-01-24 09:15:40 +0100

    Update UserFeedbackAPI.java

--- services/src/main/java/org/fao/geonet/api/userfeedback/UserFeedbackAPI.java
+++ services/src/main/java/org/fao/geonet/api/userfeedback/UserFeedbackAPI.java
@@ -493,7 +493,7 @@ public class UserFeedbackAPI {
                         null);
 
                     String catalogueName = settingManager.getValue(SYSTEM_SITE_NAME_PATH);
-                    String title = XslUtil.getIndexField(null, userFeedbackDto.getMetadataUUID(), "resourceTitle", "");
+                    String title = XslUtil.getIndexField(null, userFeedbackDto.getMetadataUUID(), "resourceTitleObject", "");
 
                     List<String> toAddress = userToNotify.stream()
                         .filter(u -> StringUtils.isNotEmpty(u.getEmail()))
@@ -625,7 +625,7 @@ public class UserFeedbackAPI {
             }
         }
 
-        String title = XslUtil.getIndexField(null, metadataUuid, "resourceTitle", "");
+        String title = XslUtil.getIndexField(null, metadataUuid, "resourceTitleObject", "");
 
         MailUtil.sendMail(new ArrayList<>(toAddress),
             String.format(

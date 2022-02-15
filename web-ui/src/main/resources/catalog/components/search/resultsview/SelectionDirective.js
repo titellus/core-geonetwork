922e9680b30411d5c5adf663c2d55bd59e86c29a
Francois Prunayre <fx.prunayre@gmail.com>
2022-02-15 13:03:23 +0100

    Manage record / add add to selection and index actions.
    No need to reload selectedCount on location change.

--- web-ui/src/main/resources/catalog/components/search/resultsview/SelectionDirective.js
+++ web-ui/src/main/resources/catalog/components/search/resultsview/SelectionDirective.js
@@ -28,10 +28,10 @@
   var module = angular.module('gn_selection_directive', []);
 
   module.directive('gnSelectionWidget', [
-    '$translate', '$http', 'hotkeys', 'gnAlertService', '$rootScope',
+    '$translate', '$http', 'hotkeys', 'gnAlertService',
     'gnHttp', 'gnMetadataActions', 'gnConfig', 'gnConfigService',
     'gnSearchSettings', 'gnSearchManagerService', 'gnCollectionService',
-    function($translate, $http, hotkeys, gnAlertService, $rootScope,
+    function($translate, $http, hotkeys, gnAlertService,
              gnHttp, gnMetadataActions, gnConfig, gnConfigService,
              gnSearchSettings, gnSearchManagerService, gnCollectionService) {
 
@@ -41,14 +41,6 @@
         templateUrl: '../../catalog/components/search/resultsview/partials/' +
             'selection-widget.html',
         link: function(scope, element, attrs) {
-          $rootScope.$on('$locationChangeSuccess', function (event, current, previous) {
-            gnSearchManagerService.selected(scope.searchResults.selectionBucket)
-              .success(function(res) {
-                if (angular.isArray(res)) {
-                  scope.searchResults.selectedCount = res.length;
-                }
-              });
-          });
           scope.customActions = gnSearchSettings.customSelectActions;
           var watchers = [];
           scope.checkAll = true;

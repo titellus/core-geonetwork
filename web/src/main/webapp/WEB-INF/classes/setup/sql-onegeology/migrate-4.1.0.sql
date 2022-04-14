6f51a69482b24410a0bd4e689a4b33b9468c078d
François Prunayre <fx.prunayre@gmail.com>
2022-04-14 15:38:20 +0200

    Update migrate-4.1.0.sql

--- web/src/main/webapp/WEB-INF/classes/setup/sql-onegeology/migrate-4.1.0.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-onegeology/migrate-4.1.0.sql
@@ -432,6 +432,11 @@ UPDATE Settings SET value='+' WHERE name='system/platform/subVersion';
 
 
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/minLength', '6', 1, 12000, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/maxLength', '20', 1, 12001, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/usePattern', 'true', 2, 12002, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal, editable) VALUES ('system/security/passwordEnforcement/pattern', '^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).*)$', 0, 12003, 'n', 'n');
+
 
 
 -- ###############

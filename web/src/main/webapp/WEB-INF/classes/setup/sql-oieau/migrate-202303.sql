e4556764f0158435534d698d1c9abdf2b8495fa7
François Prunayre <fx.prunayre@gmail.com>
2023-03-30 17:24:46 +0200

    Update migrate-202303.sql

--- web/src/main/webapp/WEB-INF/classes/setup/sql-oieau/migrate-202303.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-oieau/migrate-202303.sql
@@ -138,3 +138,5 @@ UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
 UPDATE Settings SET value='4.2.4' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+UPDATE Settings SET editable = 'y'  WHERE editable IS NULL;
